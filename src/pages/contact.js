1bcf3f772e09c1936f9b5ad1d0afefabcc3156c1
Zahra <hizahrazadeh@gmail.com>
2021-03-31 14:16:52 -0400

    changed link text

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,7 +7,7 @@ const ContactPage = () => {
             <h1>Contact Her!</h1>
             <p>Call: 450-299-2222</p>
             <p>Email: ladida@nevermind.com</p>
-            <p><Link to="https://twitter.com/icycodes"> Twitter </Link></p>
+            <p>Say hi on Twitter <Link to="https://twitter.com/icycodes"> @icycodes </Link></p>
         </div>
     )
 }
