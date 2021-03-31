892cc488a9e329504cd36bb1e84407c94288eceb
Zahra <hizahrazadeh@gmail.com>
2021-03-31 14:21:27 -0400

    created footer cmpnt

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Created by Zahra Dabirzadeh, © 2021</p>
+        </footer>
+    )
+}
+export default Footer;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby';
+import Footer from '../components/footer';
 
 const ContactPage = () => {
     return (
@@ -8,6 +9,7 @@ const ContactPage = () => {
             <p>Call: 450-299-2222</p>
             <p>Email: ladida@nevermind.com</p>
             <p>Say hi on Twitter <Link to="https://twitter.com/icycodes"> @icycodes </Link></p>
+        <Footer/>
         </div>
     )
 }
