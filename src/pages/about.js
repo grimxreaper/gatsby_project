de00f5ae46dabb268c5fcfcebd33a777c7d62d98
Zahra <hizahrazadeh@gmail.com>
2021-03-31 12:00:51 -0400

    created new pages

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <div>
+        <h1>About the Artist</h1>
+        <p>She is an artist who grew up in Rjekyavik, Iceland.</p>
+        <p>An advocate against whale hunting, and an avid free-diver.</p>
+        </div>
+    )
+}
+
+export default AboutPage;
---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h1>Contact Her!</h1>
+            <p>Call: 450-299-2222</p>
+            <p>Email: ladida@nevermind.com</p>
+        </div>
+    )
+}
+
+export default ContactPage;
