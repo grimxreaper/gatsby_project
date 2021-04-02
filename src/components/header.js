34895f607cc82bb866497de83d267dc88ad26227
Zahra <hizahrazadeh@gmail.com>
2021-04-02 09:47:48 -0400

    scss styles for nav-bar

--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,51 @@
-import React from 'react'
-import { Link } from 'gatsby';
-import * as headerStyles from './header.module.scss';
+import React from "react"
+import { Link } from "gatsby"
+import * as headerStyles from "./header.module.scss"
 
-const Header  = () => {
-    return (
-        <div>
-            <header className={headerStyles.header}>
-            <h1>
-                <Link className={headerStyles.title} to="/">Icy Codes</Link>
-            </h1>
-            <nav>
-                <ul className={headerStyles.navList}>
-                    <li><Link to="../blog">Writings</Link> </li>
-                    <li><Link to="../about">About</Link></li>
-                    <li><Link to="../contact">Get In Touch</Link> </li>
-                    <li><Link to="/">Home</Link></li>
-                </ul>
-            
-            </nav>
-            </header>
-        </div>
-    )
+const Header = () => {
+  return (
+    <div>
+      <header className={headerStyles.header}>
+        <h1>
+          <Link className={headerStyles.title} to="/">
+            Icy Codes
+          </Link>
+        </h1>
+        <nav>
+          <ul className={headerStyles.navList}>
+            <li>
+              <Link className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="../blog">
+                Writings
+              </Link>{" "}
+            </li>
+            <li>
+              <Link className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="../about">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="../contact">
+                Get In Touch
+              </Link>{" "}
+            </li>
+            <li>
+              <Link className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/">
+                Home
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  )
 }
 
-export default Header; 
+export default Header
