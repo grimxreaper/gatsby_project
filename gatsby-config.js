da32ae6e59410d8f5add63a6f68cca18b7becdd3
Zahra <hizahrazadeh@gmail.com>
2021-04-03 09:18:24 -0400

    Installing Remark, to transform markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,14 @@ module.exports = {
     author: 'Chessa'
 
   },
-  plugins: [`gatsby-plugin-sass`],
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`
+      }
+    }
+  ],
 }
