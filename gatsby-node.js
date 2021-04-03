b711b15539dc83653bb84ec825eb468fe4ff45c0
Zahra <hizahrazadeh@gmail.com>
2021-04-03 13:24:32 -0400

    Adding Async/Await

--- gatsby-node.js
+++ gatsby-node.js
@@ -18,6 +18,24 @@ module.exports.createPages = ({ graphql, actions }) => {
     const { createPage } = actions 
     const blogTemplate = path.resolve('./src/templates/blog.js')
 
+    //this graphql is not the same as the graphql
+    //we've been importing, this is a function itself that we pass a string graphql query to
+    //this graphql function actually returns a promise, and we'll be using async to handle it
+
+    graphql(`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        fields {
+                            slug
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
     //1. get path to template
     //2. get markdown data
     //3. create new pages
