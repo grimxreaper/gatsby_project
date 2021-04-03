7c6abd7c9c04021c0d0e0265cc7ae4776074780a
Zahra <hizahrazadeh@gmail.com>
2021-04-03 14:50:41 -0400

    Added & exported query so that gatsby can use it

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,6 +1,26 @@
 import React from 'react';
-import Layout from '../components/layout'
+import { graphql } from 'gatsby';
+import Layout from '../components/layout';
 
+export const query = graphql`
+query (
+    $slug: String!
+  ) {
+    markdownRemark (
+      fields: {
+        slug: {
+          eq: $slug
+        }
+      }
+    ){
+      frontmatter{
+        title
+        date
+      }
+      html
+    }
+  }
+`
 const Blog = () => {
     return (
         <Layout>
