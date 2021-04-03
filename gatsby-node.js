646766150575c56068d1891452c2bc51a691d4b2
Zahra <hizahrazadeh@gmail.com>
2021-04-03 19:54:37 -0400

    Debugging with updating contentful back to latest version and trying a new fix via the updated contentful documentation

--- gatsby-node.js
+++ gatsby-node.js
@@ -15,6 +15,19 @@ module.exports.createPages = async ({ graphql, actions }) => {
     }
   `)
 
+  const options = {
+    renderNode: {
+      [BLOCKS.EMBEDDED_ASSET]: node => {
+        const {
+          fixed: { src },
+          title,
+        } = node.data.target
+
+        return <img src={src} alt={title} />
+      },
+    },
+  }
+
   //we can now do something with the response, we want to iterate over all
   //those posts and run the createPage function for each of them
 
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 
 // export const query = graphql`
 //   query($slug: String!) {
@@ -15,21 +16,33 @@ import Layout from "../components/layout"
 // `
 
 export const query = graphql`
-  query($slug: String!) {
-    contentfulBlogPost(slug: {eq: $slug}) {
-      title
-      publishedDate(formatString: "MMMM Do, YYYY")
+query($slug: String!) {
+  contentfulBlogPost(slug: { eq: $slug }) {
+    title
+    publishedDate(formatString: "MMMM Do, YYYY")
+    body {
+      raw
+      references {
+        ... on ContentfulAsset {
+          contentful_id
+          __typename
+          title
+          fixed(width: 750) {
+            src
+          }
+        }
+      }
     }
-
   }
+}
 `
 
-const Blog = (props) => {
+const Blog = props => {
   return (
     <Layout>
       <h1>{props.data.contenfulBlogPost.title}</h1>
       <p>{props.data.contenfulBlogPost.publishedDate}</p>
-
+      {documentToReactComponents(props.data.contenfulBlogPost.body.json)}
     </Layout>
   )
 }
