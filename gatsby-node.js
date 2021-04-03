14d1565b16378412259591c8954ba8ec7b2e9dc7
Zahra <hizahrazadeh@gmail.com>
2021-04-03 13:42:08 -0400

    Dynamically Generating Blog Pages - used createPages API with graphql function - this graphql is not the same as the graphql we've been importing, this is a function itself that we pass a string graphql query to - this graphql function actually returns a promise, and we used  async to handle it

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,38 +5,52 @@ module.exports.onCreateNode = ({ node, actions }) => {
 
   if (node.internal.type === "MarkdownRemark") {
     const slug = path.basename(node.fileAbsolutePath, ".md")
-    
-    createNodeField({ //to add the new field onto the node
-        node, //the node we're trying to create the new field on
-        name: 'slug',
-        value: slug 
+
+    createNodeField({
+      //to add the new field onto the node
+      node, //the node we're trying to create the new field on
+      name: "slug",
+      value: slug,
     })
   }
 }
 
+//1. get path to template
+//2. get markdown data
+//3. create new pages
+
 module.exports.createPages = async ({ graphql, actions }) => {
-    const { createPage } = actions 
-    const blogTemplate = path.resolve('./src/templates/blog.js')
+  const { createPage } = actions
+  const blogTemplate = path.resolve("./src/templates/blog.js")
 
-    //this graphql is not the same as the graphql
-    //we've been importing, this is a function itself that we pass a string graphql query to
-    //this graphql function actually returns a promise, and we'll be using async to handle it
+  //this graphql is not the same as the graphql
+  //we've been importing, this is a function itself that we pass a string graphql query to
+  //this graphql function actually returns a promise, and we'll be using async to handle it
 
-    const response = await graphql(`
-        query {
-            allMarkdownRemark {
-                edges {
-                    node {
-                        fields {
-                            slug
-                        }
-                    }
-                }
+  const response = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
             }
+          }
         }
-    `)
+      }
+    }
+  `)
 
-    //1. get path to template
-    //2. get markdown data
-    //3. create new pages
+  //we can now do something with the response, we want to iterate over all
+  //those posts and run the createPage function for each of them
+
+  response.data.allMarkdownRemark.edges.forEach(edge => {
+    createPage({
+      component: blogTemplate,
+      path: `/blog/${edge.node.fields.slug}`,
+      context: {
+        slug: edge.node.fields.slug,
+      },
+    })
+  })
 }
