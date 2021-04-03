ffffeec133a087e185d29d9c8820b22f1da8b6dc
Zahra <hizahrazadeh@gmail.com>
2021-04-03 15:03:50 -0400

    Generating blog page data from dynamic graphql query

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,19 +1,11 @@
-import React from 'react';
-import { graphql } from 'gatsby';
-import Layout from '../components/layout';
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
 
 export const query = graphql`
-query (
-    $slug: String!
-  ) {
-    markdownRemark (
-      fields: {
-        slug: {
-          eq: $slug
-        }
-      }
-    ){
-      frontmatter{
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
         title
         date
       }
@@ -21,13 +13,14 @@ query (
     }
   }
 `
-const Blog = () => {
-    return (
-        <Layout>
-        this is the blog template
-        </Layout>
-
-    )
+const Blog = props => {
+  return (
+    <Layout>
+      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+      <p>{props.data.markdownRemark.frontmatter.date}</p>
+      <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}></div>
+    </Layout>
+  )
 }
 
 export default Blog
