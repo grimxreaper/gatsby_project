79695d4355058a90456299cc097827e049e37fdd
Zahra <hizahrazadeh@gmail.com>
2021-04-03 19:31:20 -0400

    Dynamic Pages & A certain bug with no error message

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,11 +3,6 @@ const path = require("path")
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
   const blogTemplate = path.resolve("./src/templates/blog.js")
-
-  //this graphql is not the same as the graphql
-  //we've been importing, this is a function itself that we pass a string graphql query to
-  //this graphql function actually returns a promise, and we'll be using async to handle it
-
   const response = await graphql(`
     query {
       allContentfulBlogPost {
@@ -18,17 +13,17 @@ module.exports.createPages = async ({ graphql, actions }) => {
         }
       }
     }
-  `);
+  `)
 
   //we can now do something with the response, we want to iterate over all
   //those posts and run the createPage function for each of them
 
-  response.data.allContentfulBlogPost.edges.forEach(edge => {
+  response.data.allContentfulBlogPost.edges.forEach((edge) => {
     createPage({
       component: blogTemplate,
-      path: `/blog/${edge.node.fields.slug}`,
+      path: `/blog/${edge.node.slug}`,
       context: {
-        slug: edge.node.slug,
+        slug: edge.node.slug
       },
     })
   })
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -2,23 +2,34 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
+// export const query = graphql`
+//   query($slug: String!) {
+//     markdownRemark(fields: { slug: { eq: $slug } }) {
+//       frontmatter {
+//         title
+//         date
+//       }
+//       html
+//     }
+//   }
+// `
+
 export const query = graphql`
   query($slug: String!) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      frontmatter {
-        title
-        date
-      }
-      html
+    contentfulBlogPost(slug: {eq: $slug}) {
+      title
+      publishedDate(formatString: "MMMM Do, YYYY")
     }
+
   }
 `
-const Blog = props => {
+
+const Blog = (props) => {
   return (
     <Layout>
-      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-      <p>{props.data.markdownRemark.frontmatter.date}</p>
-      <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}></div>
+      <h1>{props.data.contenfulBlogPost.title}</h1>
+      <p>{props.data.contenfulBlogPost.publishedDate}</p>
+
     </Layout>
   )
 }
