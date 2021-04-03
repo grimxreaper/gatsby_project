7bfaf8349b7e20005bd7b914b1ac299807f99893
Zahra <hizahrazadeh@gmail.com>
2021-04-03 19:03:40 -0400

    No longer need onCreateNode to generate slugs

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,19 +1,5 @@
 const path = require("path")
 
-module.exports.onCreateNode = ({ node, actions }) => {
-  const { createNodeField } = actions
-
-  if (node.internal.type === "MarkdownRemark") {
-    const slug = path.basename(node.fileAbsolutePath, ".md")
-
-    createNodeField({
-      //to add the new field onto the node
-      node, //the node we're trying to create the new field on
-      name: "slug",
-      value: slug,
-    })
-  }
-};
 
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
